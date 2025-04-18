612f0d1b9942b006d3eb3e54b123905e0da0f0ca
Prateek <109753518+Prateek-rajput-007@users.noreply.github.com>
2025-04-18 12:55:23 +0530

    Update Hero.tsx

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -93,9 +93,9 @@ function Hero() {
               <div className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10">
                 <Edit className="h-8 w-8" />
                 <h2 className="mt-4 text-xl font-bold text-black">Edit Your form</h2>
-                <p className="mt-1 text-sm text-gray-600">
-                  Easily edit your form to include additional questions, change the order of questions, or adjust the form's appearance.
-                </p>
+               <p className="mt-1 text-sm text-gray-600">
+  Easily edit your form to include additional questions, change the order of questions, or adjust the form&apos;s appearance.
+</p>
               </div>
 
               <div className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10">
